e8e61ef45e813c86de7dfe00671c5446f5d09c50
Dmitry Mikushin <dmitry@kernelgen.org>
2014-08-23 22:15:44 +0200

    Do not use format in unformatted output hook. Adding unformatted file output test.

--- hooks.f90
+++ hooks.f90
@@ -12,7 +12,7 @@ end subroutine asyncio_hook_write_default_formatted
 subroutine asyncio_hook_write_unit_unformatted(unt) bind(C)
   use iso_c_binding
   integer(c_int), value :: unt
-  write(unt,*) 'hook_write'
+  write(unt) 'hook_write'
 end subroutine asyncio_hook_write_unit_unformatted
 
 subroutine asyncio_hook_write_unit_formatted(unt, flen, frmt) bind(C)
--- main_cpu.f90
+++ main_cpu.f90
@@ -3,9 +3,13 @@ use asyncio
 
 call kernel1()
 
+open(3,file='filename',form='unformatted',status='unknown')
+
 call kernel2()
 
 call asyncio_flush()
+
+close(3)
 
 end program test
 
