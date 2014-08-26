2df8827193885e86f1a1c690b52f00efd1f85e80
Dmitry Mikushin <dmitry@kernelgen.org>
2014-08-27 01:26:06 +0200

    Implementing file open/close through asyncio

--- main_cpu.f90
+++ main_cpu.f90
@@ -3,13 +3,13 @@ use asyncio
 
 call kernel1()
 
-open(3,file='filename',form='unformatted',status='unknown')
+call asyncio_open(3,file='filename',form='unformatted',status='unknown')
 
 call kernel2()
 
 call asyncio_flush()
 
-close(3)
+call asyncio_close(3)
 
 end program test
 
